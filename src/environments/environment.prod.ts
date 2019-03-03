5afef53d19953d6818421ee6c93c0c462df133a0
ali abdullah khan <abdullah017196@gmail.com>
2019-03-03 17:05:38 -0500

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,6 @@
 export const environment = {
   production: true,
   firebase:{
-    apiKey: 'AIzaSyB49yZ3b5fa4Vn3XMG9_J1RPvhaNeBcVjk',
-    authDomain: 'urlamammia.firebaseapp.com',
-    databaseURL: 'https://urlamammia.firebaseio.com',
-    projectId: 'urlamammia',
-    storageBucket: 'urlamammia.appspot.com',
-    messagingSenderId: '572642450328'
+   
   }
 };
